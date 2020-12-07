981ff81a9f5951b927c178e5b0f08d38f3413d91
jurgen1c <jurgen1c@gmail.com>
2020-12-07 13:03:30 -0600

    Add examples and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tensorflow Practice
+
+- In this repo practice model to learn tensorflow
--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 import tensorflow as tf
 
+import numpy as np
+
 print(tf.__version__)
+
+# Tensor examples -----------------
+
+string = tf.Variable("this is a string", tf.string) 
+number = tf.Variable(324, tf.int16)
+floating = tf.Variable(3.567, tf.float64)
+
+# Rank / Degree of Tensors ----------------------
+
+rank1_tensor = tf.Variable(["Test"], tf.string) 
+rank2_tensor = tf.Variable([["test", "ok"], ["test", "yes"]], tf.string)
+
+""" Types of Tensors
+
+- Variable
+- Constant
+- Placeholder
+- SparseTensor """
+
+# Evaluating Tensors ----------------
+
+with tf.Session() as sess:
+  tensor.eval()
